b4893bc043041c09d9e7ab96405ba1b75c0ce644
Harsh Trivedi <109842753+hasrshtrivedi@users.noreply.github.com>
2022-08-25 11:51:09 +0530

    update hello.cpp

---
+++ basics/hello.cpp
@@ -0,0 +1,10 @@
+// task : Print "hii harsh"
+// live solution : 
+// https://replit.com/@harsh-tri-vedi/basics#main.cpp#embed&output
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "hii!Harsh";
+    return 0;
+}
